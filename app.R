e571f03ce99b044cff8e80372dde1e89fec1370c
cailing20 <cailing20@gmail.com>
2022-08-16 14:29:46 -0500

    Update app.R
    Add tooltip

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ library(RColorBrewer)
 library(scales)
 library(shinyjs)
 library(shinythemes)
+library(shinycssloaders)
 source('load_data.R')
 ui <- fluidPage(
     navbarPage(title = "Co-mutation check with AACR-GENIE v11.0 data",theme = shinytheme("paper"),
@@ -15,16 +16,16 @@ ui <- fluidPage(
                         radioButtons(inputId = 'screen.w.CNA',label = 'Count in copy number alterations',choices = c('no','yes'),inline = T),
                         fluidRow(
                             column(5,
-                                   h5('Number of significant hits by gene pair'),
+                                   h5('Number of cancer types with significant co-mutations by gene pair'),
                                    DT::dataTableOutput('gp-dt'),downloadButton(outputId = 'gp-download',label = 'Download gene pair-specific results')
                                    ),
                             column(7,
-                                   h5('Number of significant hits by cancer type'),
+                                   h5('Number of gene pairs with significant co-mutations by cancer type'),
                                    DT::dataTableOutput('type-dt'),downloadButton(outputId = 'type-download',label = 'Download cancer type-specific results')
                             )
                         ),
                         fluidRow(
-                            h5('All significant hits'),
+                            h5('All significant co-mutations'),
                             DT::dataTableOutput('full-dt'),downloadButton(outputId = 'full-download',label = 'Download all significant results')
                         )
                ),
@@ -40,17 +41,16 @@ ui <- fluidPage(
                         ),
                         fluidRow(
                             useShinyjs(),
-                            DT::dataTableOutput('dt'),
-                            plotlyOutput(outputId = 'scPlot',height = '900px')
+                            withSpinner(tagList(DT::dataTableOutput('dt'),plotlyOutput(outputId = 'scPlot',height = '900px')))
                         )
                ))
 )
 
 server <- function(input, output,session) {
     # screen results
-    output$`gp-dt`<-DT::renderDataTable(DT::datatable(result.list[[ifelse(input$screen.w.CNA=='yes','w','wo')]][['comut']],options = list(pageLength=5,scrollX = TRUE),filter='top',rownames = F))
-    output$`type-dt`<-DT::renderDataTable(DT::datatable(result.list[[ifelse(input$screen.w.CNA=='yes','w','wo')]][['type']],options = list(pageLength=5,scrollX = TRUE),filter='top',rownames = F))
-    output$`full-dt`<-DT::renderDataTable(DT::datatable(result.list[[ifelse(input$screen.w.CNA=='yes','w','wo')]][['full']],options = list(pageLength=5),filter='top',rownames = F))
+    output$`gp-dt`<-DT::renderDataTable(DT::datatable(result.list[[ifelse(input$screen.w.CNA=='yes','w','wo')]][['comut']],options = list(pageLength=5,scrollX = TRUE),filter='top',rownames = F,container = sketch.gp))
+    output$`type-dt`<-DT::renderDataTable(DT::datatable(result.list[[ifelse(input$screen.w.CNA=='yes','w','wo')]][['type']],options = list(pageLength=5,scrollX = TRUE),filter='top',rownames = F,container = sketch.type))
+    output$`full-dt`<-DT::renderDataTable(DT::datatable(result.list[[ifelse(input$screen.w.CNA=='yes','w','wo')]][['full']],options = list(pageLength=5),filter='top',rownames = F,container = sketch.full))
     
     output$`gp-download` <- downloadHandler(
         filename=function(){paste0('significant_counts_by_gene_pair_from_screening_',ifelse(input$screen.w.CNA=='yes','','not_'),'considering_copy_number_alterations.csv')},content = function(file){fwrite(result.list[[ifelse(input$screen.w.CNA=='yes','w','wo')]][['comut']],file)}
@@ -72,7 +72,7 @@ server <- function(input, output,session) {
     observeEvent(list(input$g1,input$g2),{shinyjs::hide('result-download')})
     observeEvent(input$submit,{
         if(input$g1==input$g2) showNotification("Please choose two different genes.")else{
-            output$dt<-DT::renderDataTable(DT::datatable({isolate(co.df())},options = list(pageLength=10),filter='top',rownames = F))
+            output$dt<-DT::renderDataTable(DT::datatable({isolate(co.df())},options = list(pageLength=10),filter='top',rownames = F,container = sketch.adhoc))
             output$scPlot<-renderPlotly(ggplotly(isolate(co.g())))
             if(input$show=='table'){
                 shinyjs::show('dt')
